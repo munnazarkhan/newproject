d53820052628e2958f887d691a9620c45569bb2b
munnazarkhan <heavenkhan30@gmail.com>
2026-06-27 17:05:23 +0530

    backend

---
+++ Cravings/server/index.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import connectDB from "./src/config/dbConnection.config.js";
+
+const app = express();
+
+//Default API
+app.get("/", (req, res) => {
+  console.log("Default Get API Hit");
+  res.json({ message: "Welcome to my first backend Project" });
+});
+
+//Default Error Handler
+
+app.use((err, req, res, next) => {
+  const ErrMessage = err.message || "Internal Server Error";
+  const ErrStausCode = err.statusCode || 500;
+
+  res.status(ErrStausCode).json({ message: ErrMessage });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("Server Started on port:", port);
+  connectDB();
+});
